c1034a7de4243eeac213c6f7c63e1de8c94fb2e6
Ayushi Gupta <ayushi.gupta.038@gmail.com>
2024-05-04 12:46:30 +0530

    Add format to news components

--- src/components/News.js
+++ src/components/News.js
@@ -2,43 +2,51 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=257146d026eb4bfb9639479eaf682ddd&page=1";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles});
+  }
+
+  handlePrevClick = ()=>{
+    console.log("Previous");
+  }
+
+  handleNextClick = ()=>{
+    console.log("Next");
+  }
+  
   render() {
     return (
       <div>
         <div className="container my-3">
           <h2>NewsFlash - Top headlines</h2>
-          <div className="row my-3">
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
+          
+          <div className="row">
+            {this.state.articles.map((element)=>{
+                return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/></div>
+
+            })}
+            
+            
           </div>
-          <div className="row my-3">
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-          </div>
-          <div className="row my-3">
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
-            <div className="col-md-4">
-              <NewsItem title="myTitle" description="myDescription"/>
-            </div>
+
+          <div className="container d-flex justify-content-between">
+            <button type='button' className='btn btn-dark' onClick={this.handlePrevClick}>&larr; Previous</button>
+            <button type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
           </div>
           
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl} = this.props; 
+    let {title, description, imageUrl, newsUrl} = this.props; 
     return (
-      <div>
+      <div className='my-3'>
             <div className="card" style={{width:"18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={imageUrl?imageUrl:"https://img.freepik.com/free-photo/white-paper-texture_1194-5998.jpg?w=740&t=st=1711971862~exp=1711972462~hmac=606fec35c59f3e1e624888b96901c3554153a75b48b20924a367cab9b81db4d4"} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="/" className="btn btn-primary">Go somewhere</a>
+                    <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read more</a>
                 </div>
             </div>
       </div>
