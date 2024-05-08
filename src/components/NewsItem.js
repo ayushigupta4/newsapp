2029b41a2a7d9259a65160290d944f74ce40a53c
Ayushi Gupta <ayushi.gupta.038@gmail.com>
2024-05-08 23:04:25 +0530

    Add author, time and source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props; 
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props; 
     return (
       <div className='my-3'>
             <div className="card">
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
                 <img src={imageUrl?imageUrl:"https://img.freepik.com/free-photo/white-paper-texture_1194-5998.jpg?w=740&t=st=1711971862~exp=1711972462~hmac=606fec35c59f3e1e624888b96901c3554153a75b48b20924a367cab9b81db4d4"} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-body-secondary">By {author?author: "Unknown"} on {new Date(date).toGMTString()}</small></p>
                     <a rel='noreferrer' href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read more</a>
                 </div>
             </div>
